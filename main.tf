75df44f86bfe09336276424bf6b7eca3f56b0962
Manikantan <manikantanmulleria049@gmail.com>
2025-07-14 18:16:01 +0530

    Add AWS provider block to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
 module "network" {
   source              = "./terraform/network"
   vpc_cidr            = var.vpc_cidr
